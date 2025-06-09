9a068e22a09bd477b92a7d86a97dad976b8466ce
8006169054 <43157353+8006169054@users.noreply.github.com>
2025-06-09 17:36:41 +0900

    init

---
+++ src/main/java/com/kclogix/KclogixApplication.java
@@ -0,0 +1,13 @@
+package com.kclogix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KclogixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KclogixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kclogix/KclogixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kclogix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KclogixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
